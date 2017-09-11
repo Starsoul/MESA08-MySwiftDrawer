5de3fb3e1fb2f61a7f366448f512ef72ef1c0772
jiangzongyi <reavter103@gmail.com>
2017-09-11 15:19:49 +0800

    v2

--- MySwiftDrawer/MyView.swift
+++ MySwiftDrawer/MyView.swift
@@ -9,18 +9,71 @@
 import Cocoa
 
 class MyView: NSView {
-
+    
+    var line:[CGPoint] = []  //array<CGpoint>
+    var lines:[[CGPoint]] = [] //array<array<CGpoint>>
+    
+        //My View   的呈現畫面
     override func draw(_ dirtyRect: NSRect) {
-
-        var myPath = NSBezierPath()
-        myPath.move(to: CGPoint(x: 0, y: 0))
-        myPath.line(to: CGPoint(x: 100, y: 100))
-        myPath.line(to: CGPoint(x:150, y:50))
+        for line in lines {
+        //一條線
+        if line.count<2 {continue}
+        let myPath = NSBezierPath()
+        myPath.move(to: line[0])
+        for i in 1..<line.count{
+            myPath.line(to: line[i])
+        }
+      
+        
         myPath.stroke()
-        
-        
-        
+        }
+    }
+    
+    override func mouseDown(with event: NSEvent) {
+        var line:[CGPoint] = []
+        let px = event.locationInWindow.x
+        let py = event.locationInWindow.y
+        line += [CGPoint(x: px, y: py)]
+        lines += [line]
         
     }
+    
+//    override func mouseUp(with event: NSEvent) {
+//        let px = event.locationInWindow.x
+//        let py = event.locationInWindow.y
+//        line += [CGPoint(x: px, y: py)]
+//        
+//    }
+    
+    override func mouseDragged(with event: NSEvent) {
+        let px = event.locationInWindow.x
+        let py = event.locationInWindow.y
+        line += [CGPoint(x: px, y: py)]
+        needsDisplay = true
+        lines[lines.count-1] += [CGPoint(x: px, y: py)]
+    }
+    
+    func clearme(){
+        lines = []
+        needsDisplay = true
+    }
+    
+    func undo(){
+        if lines.count > 1 {
+        lines.remove(at: lines.count - 1)
+        needsDisplay = true
+        }
+    }
+    
+    func redo(){
+        lines.
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
--- MySwiftDrawer/ViewController.swift
+++ MySwiftDrawer/ViewController.swift
@@ -10,15 +10,35 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    
+    @IBOutlet weak var Myview: MyView!
+    
+    
+    @IBAction func Clear(_ sender: Any) {
+        Myview.clearme()
+    }
+    
+    
+    @IBAction func undo(_ sender: Any) {
+        Myview.undo()
+    }
+    
+    @IBAction func redo(_ sender: Any) {
+        Myview.redo()
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        /*
         let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         let myview = MyView(frame: frame)
         view.addSubview(myview)
-        
-        
-       
+        */
     }
 
     override var representedObject: Any? {
